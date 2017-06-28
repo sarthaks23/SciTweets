c6a1a447d3f0e937050f38ab68d0eec5508d027f
mdmarshmallow <marcd2000@gmail.com>
2017-06-27 21:30:06 -0700

    Changed type varchar to type blob for storing summaries in DBConnect, minor fix in Filter, added a try/catch to homeservlet to accomodate for changes in other methods, changed try/catch to throws in SummarizeService, implemented database connection in TweetService.

--- scitweets/src/main/java/dbconnection/DBConnect.java
+++ scitweets/src/main/java/dbconnection/DBConnect.java
@@ -1,23 +1,31 @@
 package dbconnection;
 
+import java.io.IOException;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import compress.Compress;
 
 public class DBConnect {
 	private static String dbURL = "jdbc:mysql://localhost:3306/scitweetsdb?useSSL=false";
 	private static String dbUser = "scitweets";
 	private static String dbPass = "scitweets";
 
-	public static void insert(String url, String summary) throws SQLException {
+	public static void insert(String url, String summary) throws SQLException, IOException {
 		Connection conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
 		Statement statement = conn.createStatement();
-		statement.executeUpdate("INSERT INTO linkcache VALUES ('" + url + "','" + summary + "');");
+		statement.executeUpdate("INSERT INTO linkcache (url) VALUES ('" + url + "');");
+		PreparedStatement ps = conn.prepareStatement("UPDATE linkcache SET summary=? WHERE url='" + url + "'");
+		ps.setBytes(1, Compress.compressSummary(summary));
+		ps.execute();
 	}
 
-	public static String[] select(String url) throws SQLException {
+	public static String[] select(String url) throws SQLException, IOException {
 		Connection conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
 		Statement statement = conn.createStatement();
 		ResultSet resultset = statement.executeQuery("SELECT * FROM linkcache WHERE url='" + url + "'");
@@ -26,7 +34,8 @@ public class DBConnect {
 			return null;
 		} else {
 			results[0] = resultset.getString("url");
-			results[1] = resultset.getString("summary");
+			Blob blob = resultset.getBlob("summary");
+			results[1] = Compress.decompressSummary(blob.getBytes(1, (int) blob.length()));
 			return results;
 		}
 	}
--- scitweets/src/main/java/home/HomeServlet.java
+++ scitweets/src/main/java/home/HomeServlet.java
@@ -1,6 +1,7 @@
 package home;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,9 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import compress.Compress;
+import com.aylien.textapi.TextAPIException;
+
 import handles.AccountService;
 import tweets.TweetService;
+import twitter4j.TwitterException;
 
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = "/home")
@@ -26,7 +29,11 @@ public class HomeServlet extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.setAttribute("tweets", tweetService.retrieveTweets(request.getParameter("user")));
+		try {
+			request.setAttribute("tweets", tweetService.retrieveTweets(request.getParameter("user")));
+		} catch (SQLException | TextAPIException | TwitterException e) {
+			e.printStackTrace();
+		}
 		request.setAttribute("accounts", accountService.retrieveAccounts());
 		request.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(request, response);
 	}
