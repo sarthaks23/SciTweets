ed2dc494e925b5e72a691e7bd6baede1da875289
mdmarshmallow <marcd2000@gmail.com>
2017-07-28 20:14:30 -0700

    Added categories to the accountservice

--- scitweets/src/main/java/handles/Account.java
+++ scitweets/src/main/java/handles/Account.java
@@ -3,11 +3,13 @@ package handles;
 public class Account {
 	private String name;
 	private String username;
+	private String category;
 	
-	public Account(String name, String username) {
+	public Account(String name, String username, String category) {
 		super();
 		this.name = name;
 		this.username = username;
+		this.category = category;
 	}
 
 	public String getName() {
@@ -25,10 +27,18 @@ public class Account {
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	
+	public String getCategory() {
+		return category;
+	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
 
 	@Override
 	public String toString() {
-		return "account [name=" + name + ", username=" + username + "]";
+		return "Account [name=" + name + ", username=" + username + ", category=" + category + "]";
 	}
 	
 }
--- scitweets/src/main/java/handles/AccountService.java
+++ scitweets/src/main/java/handles/AccountService.java
@@ -16,7 +16,8 @@ public class AccountService {
 			for (int i = 0; i < rawAccounts.size(); i++) {
 				String accountName = rawAccounts.get(i)[0];
 				String accountUsername = rawAccounts.get(i)[1];
-				accounts.add(new Account(accountName, accountUsername));
+				String accountCategory = rawAccounts.get(i)[2];
+				accounts.add(new Account(accountName, accountUsername, accountCategory));
 			}
 		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
